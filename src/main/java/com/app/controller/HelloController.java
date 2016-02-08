b56a15b6d8f098345d9cd69efdc6c55a05f37336
Tyler Hayes <thayes@amplify.com>
2016-02-08 10:03:19 -0500

    Initial commit

---
+++ src/main/java/com/app/controller/HelloController.java
@@ -0,0 +1,32 @@
+package com.app.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Controller for the "Hello" page
+ */
+@Controller
+public class HelloController {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
+    @RequestMapping(value="/hello.html", method = RequestMethod.GET)
+    public ModelAndView handleRequest() {
+
+        LOGGER.info("Information!");
+        LOGGER.debug("Debug message!");
+        LOGGER.error("***ERROR***");
+        
+        ModelMap mm = new ModelMap();
+        mm.put("message", "HELLO WORLD©");  //throw in a special character to verify UTF-8 settings
+
+        return new ModelAndView("hello", mm);
+    }
+
+}
---
+++ web/view/error/general_error.ftl
@@ -0,0 +1,9 @@
+<html>
+    <head><title>Hello :: Spring Application</title></head>
+    <body>
+        <h1>Error!</h1>
+        <p>${message}</p>
+        <p>${exception}</p>
+        <pre>${stacktrace}</pre>
+     </body>
+</html>
